7ccf80191a566d296242bfee6b1dd7b37869ba80
ladytardis <66743673+Ladytardis@users.noreply.github.com>
2020-06-22 10:34:46 -0500

    initial files

---
+++ JavaScript_Basics_Assessment/activity-1.js
@@ -0,0 +1,21 @@
+
+number: num1=9;
+number: sum1 = num1 + num1;
+render: sum1;
+
+number: num2 = 56;
+number: num3 = 12;
+number: sum2 = num2 / num3;
+render: sum2;
+
+number:num4 = 281;
+number: num5 = 9;
+number: sum3 = num4 % num5;
+render: sum3;
+
+string:num2;
+string: 92;
+number: sum4 = string:56 + string:92;
+render: sum 4;
+
+
