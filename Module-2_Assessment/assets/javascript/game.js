76d64b67d65e836aaac0fcea0c64aade3d7ef576
ladytardis <66743673+Ladytardis@users.noreply.github.com>
2020-06-23 09:53:34 -0500

    game loops 1.0

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -1,15 +1,35 @@
 
-var wordList = [='EWOK', 'MANDALORIAN', 'REY', 'LUKE', 'REX', 'VADER', 
+
+// words to pick from
+var wordList = ['EWOK', 'MANDALORIAN', 'REY', 'LUKE', 'REX', 'VADER', 
     'FIVES', 'ASOKA', 'ANIKAN', 'BLASTER', 'SKYWALKER', 'GUNGAN', 'WOOKEE', 'KASHYYYK', 'NABOO', 'CLANKERS',
 'ENDOR', 'HOTH', 'PHASMA', 'KENOBI', 'FINN', 'SOLO', 'TATTOINE', 'SCARIF', 'PORG', 'TYRANUS', 'SIDIOUS', 'LIGHTSABER'
 ]
-
+// pick word
 var word = wordList[Math.floor(math.random()*(wordList.length-1))+1]
+
+//s
 
 for (var i=0; i < word.length; i++) {
     answerArray[i]='_';
 }
 
 var lettersLeft = word.length
+guesses=0
+guessesLeft=(7-guesses)
+lettersUsed =[]
 
+while (lettersLeft > 0) && (guessesLeft > 0){
 
+const myFunction = function (guess) {
+    console.log(event.key);
+    if (keyup === word){answerArray[i]=keyup
+    }
+    else {guesses++;
+         lettersUsed.push('keyup')
+    }
+}
+
+document.addEventListener('keyup', myFunction)
+
+//New Idea
